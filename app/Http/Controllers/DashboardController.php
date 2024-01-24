7b05b026959b6aebf853c08785d6b9c8b9fba3e1
wbdevmaguranye <jmaguranye@gmail.com>
2024-01-24 16:26:14 +0000

    Working with post data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,6 +11,7 @@ class DashboardController extends Controller
         $this->middleware(['auth']);
     }
     public function index(){
+        // dd(auth() -> user() -> posts);
         return view('dashboard');
     }
 }
